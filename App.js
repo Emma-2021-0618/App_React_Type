fedeee3d29a0789fce12a9b4db023607dcf24257
FrankLDF <frankluisduarte00@gmail.com>
2023-10-25 01:56:15 -0400

    adding button componet

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SearchBar from './src/components/atoms/searchComponents';
+import Button from './src/components/atoms/buttonComponent';
 
 export default function App() {
   return (
-    <SearchBar />
+    <View style={styles.container}>
+      <SearchBar />
+      <Button
+        title="Mi Botón"
+        style={{ backgroundColor: "green" }} // Personaliza los estilos aquí
+        textStyle={{ color: "white" }} // Personaliza los estilos del texto aquí
+      />
+    </View>
   );
 }
 
@@ -12,7 +20,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });
